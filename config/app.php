bf052422bdcf48b37fcd8634ea21f26af984e338
Kazuki Asakura <arthur.1121lemsche@gmail.com>
2021-03-10 20:51:35 +0900

    version 1.0.0

--- config/app.php
+++ config/app.php
@@ -10,7 +10,7 @@ return [
     | the framework needs to place the application's version in a notification
     | or any other location as required by the application or its packages.
     */
-    'version' => '0.1.3',
+    'version' => '1.0.0',
 
     /*
     |--------------------------------------------------------------------------
